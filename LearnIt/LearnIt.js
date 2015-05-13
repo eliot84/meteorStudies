514a94cf21845ccf3f96863e0f90cf30b423ab5e
Eliot84 <mr.ear84@gmail.com>
2015-05-13 17:37:51 -0400

    Adding work for functions/event use

--- LearnIt/LearnIt.js
+++ LearnIt/LearnIt.js
@@ -2,20 +2,43 @@
 myCollection = new Mongo.Collection("collected");
 
 
-// simple-todos.js
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
 
+    //theSecret = "Hello .JS is very easy when it comes to variables";
 
-//Create a multidimensional array that contains all the information from the database
+    //Create a multidimensional array that contains all the information from the database
     insideCollected: function()
     {
       return myCollection.find({});
-    }
-
-
-
-   
+    }   
   });
+
+    
+
+
+  // EVENT LISTENER ON BODY SECTION OF PAGE
+    Template.body.events({
+     "submit .new-task": function (event) {
+       // This function is called when the new task form is submitted
+
+       var w = event.target.text.value;
+
+       myCollection.insert({
+          names: w,
+          createdAt: new Date() // current time
+         });
+
+       // Clear form
+        event.target.text.value = "";
+
+        // Prevent default form submit
+       return false;
+      }
+   });
 }
+
+
+
