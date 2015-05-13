5a6c12036fbe43009caf0097791de4b2e4655ac6
Eliot84 <mr.ear84@gmail.com>
2015-05-13 11:54:33 -0400

    Adding collections work

--- LearnIt/LearnIt.js
+++ LearnIt/LearnIt.js
@@ -1,24 +1,21 @@
+
+myCollection = new Mongo.Collection("collected");
+
+
 // simple-todos.js
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
 
-    //Arrays to be Used in html for processing
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ],
 
-    names: [
-      { names: "Jack"},
-      { names: "jones"},
-      { names: "Robert"},
-      { names: "Sandra"},
-      { names: "Mike"},
-      { names: "Jane"},
-      { names: "Alice"},
-      { names: "Barbara"}
-    ]
+//Create a multidimensional array that contains all the information from the database
+    insideCollected: function()
+    {
+      return myCollection.find({});
+    }
+
+
+
+   
   });
 }
